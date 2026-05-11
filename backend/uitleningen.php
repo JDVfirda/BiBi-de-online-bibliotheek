d8cd7e72cff419559a09d68351ff8c179d671850
EvelienJ29 <evelienjelsma@gmail.com>
2026-05-11 09:17:40 +0200

    login.php

--- backend/uitleningen.php
+++ backend/uitleningen.php
@@ -1,3 +1,30 @@
 <?php
 // Een programma waarmee je een uitlening kunt ophalen (GET) uit de database
 // toevoegen (POST) en verwijderen (DELETE)
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+
+
+require_once 'config/database.php';
+
+$methode = $_SERVER['REQUEST_METHOD'];
+
+switch ($methode) {
+
+
+    case 'GET':
+    
+
+
+    case 'POST':
+
+
+
+    case 'DELETE':
+
+
+
+$verbinding->close();
